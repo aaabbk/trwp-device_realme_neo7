8026ed8630285eb67c0ffcb14e5eeaaf46aba820
aaabbk <2268951925@qq.com>
2025-09-24 17:48:36 +0800

    fix error

--- device.mk
+++ device.mk
@@ -7,36 +7,24 @@
 
 LOCAL_PATH := device/oplus/ossi
 
+# 基本产品配置
+PRODUCT_DEVICE := ossi
+PRODUCT_NAME := omni_ossi
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX5060
+PRODUCT_MANUFACTURER := realme
+
+# 架构支持
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # A/B
+AB_OTA_UPDATER := true
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
-
-# Boot control HAL - 使用恢复模式专用版本
-PRODUCT_PACKAGES += \
-    android.hardware.boot-service.mtk_recovery \
-    android.hardware.boot@1.0 \
-    android.hardware.boot@1.1 \
-    android.hardware.boot@1.2
-
-PRODUCT_PACKAGES += \
-    bootctrl.common
-
-# 删除过时的静态 HAL 配置
-# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-#     bootctrl.common \
-#     libgptutils \
-#     libz \
-#     libcutils
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -50,8 +38,39 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot-service.mtk_recovery \
+    android.hardware.boot@1.0 \
+    android.hardware.boot@1.1 \
+    android.hardware.boot@1.2
+
+PRODUCT_PACKAGES += \
+    bootctrl.common
+
+# Vendor Boot 支持（关键添加）
+PRODUCT_PACKAGES += \
+    vendor_bootimage
+
+# 恢复模式工具
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
 # 动态分区
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# 启用 vendor_boot 构建
+PRODUCT_BUILD_VENDOR_BOOT_IMAGE := true
+
 # Soong 命名空间
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
+# 构建属性
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="ossi-user 14 UKQ1.231108.001 1755266220662 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
